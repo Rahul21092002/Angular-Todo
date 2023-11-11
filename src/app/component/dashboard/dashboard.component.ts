6498dcbff8a25a5f81bda63b75c458e40da85a52
R Rahul <rahulramu21092002@gmail.com>
2023-11-12 01:06:54 +0530

    first and final commit

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  taskObj : Task = new Task
+  taskArr : Task[] = []
+
+  addTaskValue : string = ""
+  editTaskValue : string = ""
+
+  constructor(private crudService : CrudService) { }
+
+  ngOnInit(): void {
+    this.editTaskValue = ""
+    this.taskObj = new Task()
+    this.taskArr = []
+    this.getAllTask()
+  }
+
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res
+    },err => {
+      alert('Failed to get all task')
+    })
+  }
+
+  addTask(){
+    if (this.addTaskValue.trim() === '') {
+      alert("Input Field cannot be empty")
+    }else {
+      this.taskObj.task_name = this.addTaskValue
+    this.crudService.addTask(this.taskObj).subscribe(res => {
+      this.ngOnInit()
+      this.addTaskValue = ""
+    }, err => {
+      alert('Failed to add task')
+    })
+    }
+  }
+
+  editTask() {
+    this.taskObj.task_name = this.editTaskValue
+    this.crudService.editTask(this.taskObj).subscribe(res => {
+      this.ngOnInit()
+    },err => {
+      alert('Failed to edit the task')
+    })
+  }
+
+  deleteTask(etask : Task){
+    this.crudService.deleteTask(etask).subscribe(res => {
+      this.ngOnInit()
+    },err => {
+      alert("Failed to delete Task")
+    })
+  }
+
+  call(etask : Task ){
+    this.taskObj = etask
+    this.editTaskValue = etask.task_name
+  }
+
+  
+
+
+}
